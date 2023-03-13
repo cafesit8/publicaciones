4effd6b5096eacf18e780d453339a5acd1fb7d55
cafesito <leliasgomer@gmail.com>
2023-03-12 23:25:42 -0500

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/publicaciones/",
+  build: {
+    chunkSizeWarningLimit: 1000
+  },
 })
 
 // import { dependencies } from './package.json';
