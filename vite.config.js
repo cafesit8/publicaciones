6804a0f0ff3043a88f13057f1e983b40d5f86717
cafesito <leliasgomer@gmail.com>
2023-03-12 20:48:47 -0500

    chunks problem fixed

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/publicaciones/",
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "node_modules"),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
