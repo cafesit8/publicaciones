8ecf88a5096b9feddad40af72702558b954b33e5
cafesito <leliasgomer@gmail.com>
2023-03-12 23:24:35 -0500

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,34 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/publicaciones/",
-// })
-
-import { dependencies } from './package.json';
-function renderChunks(deps) {
-  let chunks = {};
-  Object.keys(deps).forEach((key) => {
-    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
-    chunks[key] = [key];
-  });
-  return chunks;
-}
-// https://vitejs.dev/config/
+//https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [React()],
+  plugins: [react()],
   base: "/publicaciones/",
-  build: {
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-router-dom', 'react-dom'],
-          ...renderChunks(dependencies),
-        },
-      },
-    },
-  },
-  })
+})
+
+// import { dependencies } from './package.json';
+// function renderChunks(deps) {
+//   let chunks = {};
+//   Object.keys(deps).forEach((key) => {
+//     if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+//     chunks[key] = [key];
+//   });
+//   return chunks;
+// }
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [React()],
+//   base: "/publicaciones/",
+//   build: {
+//     sourcemap: false,
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           vendor: ['react', 'react-router-dom', 'react-dom'],
+//           ...renderChunks(dependencies),
+//         },
+//       },
+//     },
+//   },
+// })
